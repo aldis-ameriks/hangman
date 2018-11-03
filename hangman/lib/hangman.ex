db028700b7ff280f96c1ba0d5957455978a8b8d2
Aldis Ameriks <aldis.ameriks@gmail.com>
2018-11-03 13:21:28 +0200

    Reformat code with `mix format`

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -1,16 +1,13 @@
 defmodule Hangman do
-  alias Hangman.Game
-
   def new_game() do
-  	Hangman.Server.start_link()
+    Hangman.Server.start_link()
   end
 
   def tally(game_pid) do
-  	GenServer.call(game_pid, { :tally })
+    GenServer.call(game_pid, {:tally})
   end
 
   def make_move(game_pid, guess) do
-  	GenServer.call(game_pid, { :make_move, guess })
+    GenServer.call(game_pid, {:make_move, guess})
   end
-
 end
--- hangman/lib/hangman/game.ex
+++ hangman/lib/hangman/game.ex
@@ -23,7 +23,6 @@ defmodule Hangman.Game do
   def make_move(game, guess) do
     validate_guess(game, guess, is_single_character(guess))
     |> return_with_tally
-
   end
 
   def tally(game) do
@@ -88,6 +87,6 @@ defmodule Hangman.Game do
   defp maybe_won(_), do: :good_guess
 
   defp return_with_tally(game) do
-    { game, tally(game) }
+    {game, tally(game)}
   end
 end
