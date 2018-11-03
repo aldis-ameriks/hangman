06e500a9274e8c8c9b154c001fde68bb85390b51
Aldis Ameriks <aldis.ameriks@gmail.com>
2018-11-03 13:21:00 +0200

    Update API to use GenServer

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -1,8 +1,16 @@
 defmodule Hangman do
   alias Hangman.Game
 
-  defdelegate new_game(), to: Game
-  defdelegate tally(game), to: Game
-  defdelegate make_move(game, guess), to: Game
+  def new_game() do
+  	Hangman.Server.start_link()
+  end
+
+  def tally(game_pid) do
+  	GenServer.call(game_pid, { :tally })
+  end
+
+  def make_move(game_pid, guess) do
+  	GenServer.call(game_pid, { :make_move, guess })
+  end
 
 end
