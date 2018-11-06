42a062fe6c62da00179c21dee0a4225dd131fb95
Aldis Ameriks <aldis.ameriks@gmail.com>
2018-11-06 15:10:23 +0200

    Start Hangman server separately by doing rpc

--- text_client/lib/text_client/interact.ex
+++ text_client/lib/text_client/interact.ex
@@ -1,9 +1,10 @@
 defmodule TextClient.Interact do
 
+  @hangman_server :hangman@AMAC02S8733G8WN
   alias TextClient.{Player, State}
 
   def start() do
-    Hangman.new_game()
+    new_game()
     |> setup_state()
     |> Player.play()
   end
@@ -13,5 +14,10 @@ defmodule TextClient.Interact do
       game_service: game,
       tally: Hangman.tally(game)
     }
+  end
+
+  defp new_game() do
+    Node.connect(@hangman_server)
+    :rpc.call(@hangman_server, Hangman, :new_game, [])
   end
 end
