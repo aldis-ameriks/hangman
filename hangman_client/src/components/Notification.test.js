04808a79fe418ff6c7c099431b8d6a477d9ebd2c
Aldis Ameriks <aldis.ameriks@gmail.com>
2018-11-10 16:36:40 +0200

    Add snapshot tests for Notification

---
+++ hangman_client/src/components/Notification.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render } from 'react-testing-library';
+import Notification from './Notification';
+
+describe('Notification', () => {
+  ['error', 'success', 'warning', 'info'].forEach(variant => {
+    it(`${variant} is rendered correctly`, () => {
+      const component = render(
+        <Notification variant={variant}>
+          <div>Child</div>
+        </Notification>
+      );
+      expect(component.baseElement).toMatchSnapshot();
+    });
+  });
+});
