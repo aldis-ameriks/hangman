8a404eb6eecbf4785db02402cde42379abd6f981
Aldis Ameriks <aldis.ameriks@gmail.com>
2018-11-14 19:00:46 +0200

    Remove unused gallows svg

--- hangman_client/src/App.tsx
+++ hangman_client/src/App.tsx
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
-
 import { inject, observer } from 'mobx-react';
+import React, { Component } from 'react';
 import GameInput from './hangman/GameInput';
 import GameState from './hangman/GameState';
 import HangmanStore from './hangman/HangmanStore';
