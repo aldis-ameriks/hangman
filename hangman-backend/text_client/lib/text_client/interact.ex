3c6f645a42119ead74c873b896d3b1a3e4065cf6
Aldis Ameriks <aldis.ameriks@gmail.com>
2018-11-15 23:47:47 +0200

    Fix text_client to work with refactored Hangman app

--- hangman-backend/text_client/lib/text_client/interact.ex
+++ hangman-backend/text_client/lib/text_client/interact.ex
@@ -1,9 +1,8 @@
 defmodule TextClient.Interact do
-  @hangman_server :hangman@AMAC02S8733G8WN
   alias TextClient.{Player, State}
 
   def start() do
-    new_game()
+    Hangman.new_game()
     |> setup_state()
     |> Player.play()
   end
@@ -13,10 +12,5 @@ defmodule TextClient.Interact do
       game_service: game,
       tally: Hangman.tally(game)
     }
-  end
-
-  defp new_game() do
-    Node.connect(@hangman_server)
-    :rpc.call(@hangman_server, Hangman, :new_game, [])
   end
 end
