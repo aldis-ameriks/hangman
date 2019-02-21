c8aeac17fd0f954dcaf759c94e25dadfff24ec98
Aldis Ameriks <aldis.ameriks@gmail.com>
2019-02-21 16:11:42 +0200

    Add google analytics

--- hangman-client/src/index.tsx
+++ hangman-client/src/index.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { Provider } from 'mobx-react';
+import ReactGA from 'react-ga';
 import App from './App';
 import HangmanStore from './hangman/HangmanStore';
+
+if (process.env.REACT_APP_GA_TRACKING_ID) {
+  ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID);
+  ReactGA.pageview(window.location.pathname + window.location.search);
+}
 
 const hangmanStore = new HangmanStore();
 
